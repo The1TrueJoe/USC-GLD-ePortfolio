18d7f9fcd38c280843f778523511eb21d1cc10b1
Joseph Telaak <jttelaak@hotmail.com>
2024-08-20 17:39:30 -0400

    agian

--- script.js
+++ script.js
@@ -31,27 +31,29 @@ window.addEventListener('scroll', function() {
 });
 
 function openTab(evt, tabName) {
-    // Declare all variables
     var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
+    // Hide all tab contents
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
+    // Remove the active class from all tab buttons
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
+    // Show the selected tab content and add the active class to the clicked tab button
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
-// Set default tab open
+// Set the default tab to be open
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector(".tablinks").click();
+    const defaultTab = document.querySelector(".tablinks");
+    if (defaultTab) {
+        defaultTab.click();
+    }
 });
